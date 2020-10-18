f0e227ae8835527c1189a3b5b67b3d022e5df1d5
Jack Jones <44882194+jonesjacklewis@users.noreply.github.com>
2020-10-18 10:23:24 +0100

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -5,15 +5,14 @@ output FizzBuzz otherwise output the number"""
 
 def fizz_buzz(x, y):  # Creating a FizzBuzz function, for numbers from x to y
     for i in range(x, y + 1):  # For numbers between x to y:
-        out = ""
-        if i % 3 != 0 and i % 5 != 0:  # if i is not a multiple of 3 or 5
-            out += str(i)  # append the string form if i to out
-        else:  # otherwise
-            if i % 3 == 0:  # if i is a multiple of 3
-                out += "Fizz"  # append the string Fizz to out
-            if i % 5 == 0:  # if i is a multiple of 5
-                out += "Buzz"
-        print(out)  # output the value of out
+        out = ""  # Default output value
+        if i % 3 == 0:  # If i is a multiple of 3
+            out += "Fizz"  # Append Fizz to out
+        if i % 5 == 0:  # If i is a multiple of 5
+            out += "Buzz"  # Append Buzz to out
+        if out == "":  #  If nothing has been appended to out
+            out = str(i)  # Set out to string representation of i
+        print(out)  # print the value of out
 
 
-fizz_buzz(1, 20)  # perform the FizzBuzz function for numbers between 1 and 100
+fizz_buzz(1, 100)  # perform the FizzBuzz function for numbers between 1 and 100
